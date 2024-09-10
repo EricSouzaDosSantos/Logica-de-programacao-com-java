b6edbb50b2c906e69a3c0d5578214edb716f3186
Eric <ericsouza94895@gmail.com>
2024-09-10 16:58:24 -0300

    feat: exercise 8: calculate the area  ​​a rectangle

---
+++ src/Examples/rectangle.java
@@ -0,0 +1,31 @@
+package Examples;
+
+public class rectangle {
+
+    private float width;
+
+    private float height;
+
+    public rectangle(){
+
+    }
+    public float CalcAreaRetangulo(){
+        float area = width * height;
+        return area;
+    }
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+}
