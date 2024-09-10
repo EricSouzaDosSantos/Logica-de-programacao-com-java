95b5d792692baf98c077a8223bc94433f345d254
Eric <ericsouza94895@gmail.com>
2024-09-10 16:53:19 -0300

    feat: exercise 4: Explicit casting

---
+++ src/Examples/CastingExplicit.java
@@ -0,0 +1,26 @@
+package Examples;
+
+public class CastingExplicit {
+    private double valueDouble;
+
+    private int valueInt;
+
+    public CastingExplicit() {
+    }
+
+    public double getValueDouble() {
+        return valueDouble;
+    }
+
+    public void setValueDouble(double valueDouble) {
+        this.valueDouble = valueDouble;
+    }
+
+    public int getValueInt() {
+        return valueInt;
+    }
+
+    public void setValueInt(int valueInt) {
+        this.valueInt = valueInt;
+    }
+}
