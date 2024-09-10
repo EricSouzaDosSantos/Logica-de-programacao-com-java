8106c23b71f7370f707f8c9121a3022d31fce918
Eric <ericsouza94895@gmail.com>
2024-09-10 16:49:59 -0300

    feat: exercise 1: declaration and initialization of variables

---
+++ src/Examples/People.java
@@ -0,0 +1,48 @@
+package Examples;
+
+public class People {
+
+    private String name;
+    private int old;
+    private double weight;
+    private float height;
+
+    public People(){
+        this.name = name;
+        this.old = old;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getOld(){
+        return old;
+    }
+
+    public void setOld(int old) {
+        this.old = old;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+}
