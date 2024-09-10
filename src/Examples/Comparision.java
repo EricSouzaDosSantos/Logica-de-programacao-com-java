ae74b9cdf18fe5fb5175d9f6c8d7c1dd8e9f1c91
Eric <ericsouza94895@gmail.com>
2024-09-10 16:55:47 -0300

    feat: exercise 6: comparison operators

---
+++ src/Examples/Comparision.java
@@ -0,0 +1,45 @@
+package Examples;
+
+public class Comparision {
+
+    private int value1;
+
+    private int value2;
+
+    private boolean comparingNumber;
+
+    public Comparision(){
+
+    }
+
+    public void ComparingNumbers(){
+        comparingNumber = value1 == value2;
+        System.out.println(value1+" é igual "+value2+": "+comparingNumber);
+        comparingNumber = value1 != value2;
+        System.out.println(value1+" é diferente "+value2+": "+comparingNumber);
+        comparingNumber = value1 > value2;
+        System.out.println(value1+" é maior "+value2+": "+comparingNumber);
+        comparingNumber = value1 < value2;
+        System.out.println(value1+" é menor "+value2+": "+comparingNumber);
+        comparingNumber = value1 >= value2;
+        System.out.println(value1+" é maior ou igual "+value2+": "+comparingNumber);
+        comparingNumber = value1 <= value2;
+        System.out.println(value1+" é menor ou igual "+value2+": "+comparingNumber);
+    }
+
+    public int getValue1() {
+        return value1;
+    }
+
+    public void setValue1(int value1) {
+        this.value1 = value1;
+    }
+
+    public int getValue2() {
+        return value2;
+    }
+
+    public void setValue2(int value2) {
+        this.value2 = value2;
+    }
+}
