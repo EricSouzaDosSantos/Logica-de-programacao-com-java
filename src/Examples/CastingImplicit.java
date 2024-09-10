b1a077b83eec7f8c866606c42c77045457d759c6
Eric <ericsouza94895@gmail.com>
2024-09-10 16:52:29 -0300

    feat: exercise 3: implicit casting

---
+++ src/Examples/CastingImplicit.java
@@ -0,0 +1,29 @@
+package Examples;
+
+public class CastingImplicit {
+
+    private int valueInt;
+
+    private double valueDouble;
+
+    public CastingImplicit() {
+        this.valueDouble = valueDouble;
+        this.valueInt = valueInt;
+    }
+
+    public int getValueInt() {
+        return valueInt;
+    }
+
+    public void setValueInt(int valueInt) {
+        this.valueInt = valueInt;
+    }
+
+    public double getValueDouble() {
+        return valueDouble;
+    }
+
+    public void setValueDouble(double valueDouble) {
+        this.valueDouble = valueDouble;
+    }
+}
